a8a4c3414ead8214cdbe0e7978693d1d95e7fe1d
Kevin Cua <kevincua@Kevins-MacBook-Pro.local>
2018-09-11 21:28:41 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people, I'm Kevin Dizon-Cuae</h1>
+    <p>I'm here to learn.</p>
+    <p>I'm here to have new experiences.</p>
+    <p>Let's see how far we can go.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
